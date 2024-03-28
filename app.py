8b763c1dc81f740b6c5f43ef64dd38299ddad1f1
l4b13 <vlad_dkfl@bk.ru>
2024-03-28 16:53:07 +0900

    qwerty

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ app = Flask(__name__)
 @app.route("/")
 def index_page():
     return "Hello, World!"
+
--- test_app.py
+++ test_app.py
@@ -14,3 +14,4 @@ def test_index_page(client):
     res = client.get('/')
     assert res.status_code == 200
     assert 'Hello, World' in res.text
+
